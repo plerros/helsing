plerros/helsing

commit dbc1f955e9e794073ec32c0c2a9143729f98795a

clean up alg_cache

helsing/src/vampire/vargs.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
  * Copyright (c) 2012 Jens Kruse Andersen
- * Copyright (c) 2021-2022 Pierro Zachareas
+ * Copyright (c) 2021-2023 Pierro Zachareas
  */
 
 #include <stdlib.h>
@@ -229,7 +229,10 @@ static void alg_cache_init(struct alg_cache *ptr, fang_t mod, struct cache *cach
 {
 	OPTIONAL_ASSERT(ptr != NULL);
 	ptr->mod = mod;
-	ptr->digits_array = cache->dig;
+
+	ptr->digits_array = NULL;
+	if (cache != NULL)
+		ptr->digits_array = cache->dig;
 }
 
 static void alg_cache_split(
@@ -281,41 +284,46 @@ static void alg_cache_set(
 	 * x >= n+1 - x
 	 */
 
+	if (ptr->multiplicand[0].iterator != BASE-1)
+		abort();	// Let the compiler know that this is constant
 	OPTIONAL_ASSERT(ptr->product[3 - 1].iterator == 0);
 }
 
 static void alg_cache_check(struct alg_cache *ptr, int *result)
 {
 	const digits_t *digits_array = ptr->digits_array;
-	if (
-		ptr->dig_multiplier
-		+ digits_array[ptr->multiplicand[0].number]
-		+ digits_array[ptr->multiplicand[1].number]
-		==
-		digits_array[ptr->product[0].number]
-		+ digits_array[ptr->product[1].number]
-		+ digits_array[ptr->product[2].number]
-	)
+
+	digits_t a = ptr->dig_multiplier;
+	for (int i = 0; i < 2; i++)
+		a += digits_array[ptr->multiplicand[i].number];
+
+	digits_t b = digits_array[ptr->product[0].number];
+	for (int i = 1; i < 3; i++)
+		b += digits_array[ptr->product[i].number];
+
+	if (a == b)
 		(*result) += 1;
 }
 
 static inline void alg_cache_iterate(
 	fang_t mod,
 	struct num_part *arr,
-	fang_t iterator)
+	int elements)
 {
-	arr[0].number += iterator;
-	if (arr[0].number >= mod) {
-		arr[0].number -= mod;
-		arr[1].number += 1;
+	int i = 0;
+	for (; i < elements - 1; i++) {
+		arr[i].number += arr[i].iterator;
+		if (arr[i].number >= mod) {
+			arr[i].number -= mod;
+			arr[i + 1].number += 1;
+		}
 	}
 }
 
 static void alg_cache_iterate_all(struct alg_cache *ptr)
 {
-	alg_cache_iterate(ptr->mod, ptr->multiplicand, BASE - 1);
-	alg_cache_iterate(ptr->mod, ptr->product, ptr->product[0].iterator);
-	alg_cache_iterate(ptr->mod, &(ptr->product[1]), ptr->product[1].iterator);
+	alg_cache_iterate(ptr->mod, ptr->multiplicand, 2);
+	alg_cache_iterate(ptr->mod, ptr->product, 3);
 }
 
 #endif /* ALG_CACHE */

commit 5937e7cc57896c6ddf716070956ee5abfcc0eacd

Fix performance issue introduced by dbc1f955e9e794073ec32c0c2a9143729f98795a
When compiled with clang, it would perform  25~60% slower. Below are some benchmarks I ran. rfact0 means the refactor branch without this commit, and rfact1 means with this commit.

r9 7900, fedora 39
	gcc 13.2.1
		-n 12
			1.0	2.803 s ±  0.010 s
			1.0.2	3.076 s ±  0.011 s
			1.1.1	2.849 s ±  0.008 s
			1.1.2	2.855 s ±  0.014 s
			rfact0	2.807 s ±  0.011 s
			rfact2 	2.837 s ±  0.018 s
	clang  17.0.4
		-n 12
			1.0	3.452 s ±  0.020 s
			1.0.2	3.472 s ±  0.014 s
			1.1.1	3.708 s ±  0.018 s
			1.1.2	3.700 s ±  0.016 s
			rfact0	5.439 s ±  0.026 s
			rfact1	3.665 s ±  0.014 s
i3 10110u, debian 11
	gcc 12.2.0
		-n 10
			1.0	656.0 s ±  3.1 ms
			1.0.2	513.4 s ±  2.4 ms
			1.1.1	606.3 s ±  2.2 ms
			1.1.2	494.1 s ±  3.1 ms
			rfact0	507.2 s ±  3.0 ms
			rfact2 	519.9 s ±  2.1 ms
	clang  14.0.6
		-n 10
			1.0	533.6 s ±  2.0 ms
			1.0.2	533.0 s ±  3.2 ms
			1.1.1	617.4 s ±  1.6 ms
			1.1.2	586.0 s ±  2.8 ms
			rfact0	949.2 s ±  3.4 ms
			rfact1	602.5 s ±  1.7 ms

rpi-zero w, debian 11, CACHE_SIZE=64
	gcc 10.2.1
		make
			main	17.776 s
			rfact0	16.299 s
			rfact1 	17.940 s
		-n 8
			main	269.3 ms ±  2.2 ms
			rfact0	256.9 ms ±  0.9 ms
			rfact1	269.1 ms ±  2.1 ms
		-n 10
			main	22.012 s ±  0.082 s
			rfact0	20.837 s ±  0.052 s
			rfact1 	23.190 s ±  0.058 s
	clang 11.0.1-2
		make
			main	21.967 s
			rfact0	22.287 s
			rfact1 	26.001 s
		-n 8
			main	292.3 ms ±  1.3 ms
			rfact0	361.9 ms ±  1.6 ms
			rfact1	274.9 ms ±  1.2 ms
		-n 10
			main	25.059 s ±  0.118 s
			rfact0	33.823 s ±  0.079 s
			rfact1 	23.187 s ±  0.057 s

rpi-zero w, debian 11, CACHE_SIZE=32
	gcc
		make
			main	16.473 s
			rfact0	16.625 s
			rfact1 	17.226 s
		-n 8
			main	190.1 ms ±  1.4 ms
			rfact0	177.1 ms ±  1.0 ms
			rfact1	201.7 ms ±  0.9 ms
	clang
		make
			main	21.652 s
			rfact0	22.140 s
			rfact1 	22.349 s
		-n 8
			main	186.9 ms ±  2.1 ms
			rfact0	258.0 ms ±  0.8 ms
			rfact1	188.4 ms ±  1.4 ms

helsing/src/vampire/vargs.c
@@ -213,6 +213,7 @@ struct num_part
 {
 	fang_t number;
 	fang_t iterator;
+	fang_t carry;
 };
 
 struct alg_cache
@@ -241,7 +242,7 @@ static void alg_cache_split(
 	vamp_t iterator,
 	struct num_part *arr,
 	size_t n)
-{	
+{
 	if (n == 0)
 		return;
 
@@ -250,9 +251,11 @@ static void alg_cache_split(
 		number /= mod;
 		arr[i].iterator = iterator % mod;
 		iterator /= mod;
+		arr[i].carry = 0;
 	}
 	arr[n - 1].number = number; // number >= number % mod
 	arr[n - 1].iterator = iterator;
+	arr[n - 1].carry = 0;
 }
 
 static void alg_cache_set(
@@ -310,14 +313,20 @@ static inline void alg_cache_iterate(
 	struct num_part *arr,
 	int elements)
 {
-	int i = 0;
-	for (; i < elements - 1; i++) {
+	// Splitting the code to multiple loops seems to help compilers optimize it
+
+	for (int i = 0; i < elements - 1; i++)
 		arr[i].number += arr[i].iterator;
+
+	for (int i = 0; i < elements - 1; i++) {
+		arr[i].carry = 0;
 		if (arr[i].number >= mod) {
 			arr[i].number -= mod;
-			arr[i + 1].number += 1;
+			arr[i].carry = 1;
 		}
 	}
+	for (int i = 0; i < elements - 1; i++)
+		arr[i + 1].number += arr[i].carry;
 }
 
 static void alg_cache_iterate_all(struct alg_cache *ptr)